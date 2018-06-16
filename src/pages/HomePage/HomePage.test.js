d6716dd8e4a6a1a77fb30ad7f40a4233b0e7c5f9
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-06-16 16:48:42 -0300

    fix: test wrong input parameter in HomePage component;

--- src/pages/HomePage/HomePage.test.js
+++ src/pages/HomePage/HomePage.test.js
@@ -1,6 +1,7 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import HomePage from './HomePage';
+import { Title } from './HomePage.styled';
 
 it('renders without crashing', () => {
   shallow(<HomePage />);
@@ -8,7 +9,7 @@ it('renders without crashing', () => {
 
 it('renders welcome message', () => {
   const wrapper = shallow(<HomePage />);
-  const welcome = <h2>Welcome to React</h2>;
+  const welcome = <Title>Welcome to React</Title>;
   // expect(wrapper.contains(welcome)).to.equal(true);
   expect(wrapper.contains(welcome)).toEqual(true);
 });

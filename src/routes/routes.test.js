187873cd36bebca8ae0a9d72e77f34c664c9b09f
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-10-24 21:52:31 +0200

    chore: use jsx for react components;

--- src/routes/routes.test.js
+++ src/routes/routes.test.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { AppRouter } from './index';
+import { AppRouter } from '.';
 
 describe('App component', () => {
   const history = {

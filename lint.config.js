3ffd12b98ea52d1665916635fd9898b109594559
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-11-18 20:08:24 +0100

    chore: renamed lint.config.js jest.eslint.config.js

--- lint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  displayName: 'lint',
-  runner: 'jest-runner-eslint',
-  testMatch: ['<rootDir>/src/**/*.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/coverage/', '/dist/', '/public/']
-};

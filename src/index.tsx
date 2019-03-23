ba63431d81b95b216d124361accdc106e9256faf
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-03-23 19:30:14 +0100

    feat: make the whole app act as a module

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,44 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+
+import AppRouter from './routes';
+import createStore from './store';
+
+import * as appStores from './store/modules';
+import * as serviceWorker from './serviceWorker';
 
 import * as authModule from './modules/auth';
 import * as mainModule from './modules/main';
 
-import AppRouter from './routes';
-
-import * as serviceWorker from './serviceWorker';
-import createStore from './store';
-import * as stores from './store/modules';
-
 export type AppState = authModule.State & {
-  loading: stores.loading.State;
+  loading: appStores.loading.State;
 };
 
-const { store, history } = createStore([...authModule.stores, ...Object.values(stores)]);
+export const stores = [...authModule.stores, ...Object.values(appStores)];
+export const routes = [...mainModule.routes, ...authModule.routes];
 
-const app = (
-  <Provider store={store}>
-    <AppRouter history={history} routes={[mainModule.routes, authModule.routes]} />
-  </Provider>
-);
+const start = () => {
+  const { store, history } = createStore(stores);
 
-const root = document.querySelector('#root') as HTMLElement;
+  const app = (
+    <Provider store={store}>
+      <AppRouter history={history} routes={routes} />
+    </Provider>
+  );
 
+  render(app, document.querySelector('#root'));
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: http://bit.ly/CRA-PWA
+  serviceWorker.register();
+};
+
+// You can change this to make this app as a module them disable the
+// start in when in production
 if (process.env.NODE_ENV === 'production') {
-  // If we're running in production, we use hydrate to get fast page loads by just
-  // attaching event listeners after the initial render
-  hydrate(app, root);
+  start();
 } else {
-  // If we're not running on the server, just render like normal
-  render(app, root);
+  start();
 }
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();

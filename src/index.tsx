d18978fd848be482963705db6bffdb183e89ad50
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-03-24 11:18:12 +0100

    chore: simplified index.js

--- src/index.tsx
+++ src/index.tsx
@@ -8,27 +8,17 @@ import createStore from './store';
 
 import * as serviceWorker from './serviceWorker';
 
-const start = () => {
-  const { store, history } = createStore(stores);
+const { store, history } = createStore(stores);
 
-  const app = (
-    <Provider store={store}>
-      <AppRouter history={history} routes={routes} />
-    </Provider>
-  );
+const app = (
+  <Provider store={store}>
+    <AppRouter history={history} routes={routes} />
+  </Provider>
+);
 
-  render(app, document.querySelector('#root'));
+render(app, document.querySelector('#root'));
 
-  // If you want your app to work offline and load faster, you can change
-  // unregister() to register() below. Note this comes with some pitfalls.
-  // Learn more about service workers: http://bit.ly/CRA-PWA
-  serviceWorker.register();
-};
-
-// You can change this to make this app as a module them disable the
-// start in when in production
-if (process.env.NODE_ENV === 'production') {
-  start();
-} else {
-  start();
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();

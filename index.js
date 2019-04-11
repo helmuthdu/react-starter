4341482b4abcfb864961bcb5e1b57f7c15b23fdc
h.saatkamp <helmuth.saatkamp@leverton.ai>
2019-04-11 14:28:33 +0200

    chore: code changes

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import * as authModule from './src/modules/auth';
-import * as rootModule from './src/modules/root';
-
-export const routes = [...rootModule.routes, ...authModule.routes];
-export const stores = [...authModule.stores];
-
-export default {
-  routes,
-  stores
-};
+export * from './src/modules';

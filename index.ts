aa9c48f03c918f428f404a7dbb1406b4b90b7028
h.saatkamp <helmuth.saatkamp@leverton.ai>
2019-04-11 10:24:17 +0200

    chore: update packages.json feat: added new index file to project root;

---
+++ index.ts
@@ -0,0 +1,7 @@
+import * as rootModule from './src/modules/root';
+import * as authModule from './src/modules/auth';
+
+export type State = authModule.State;
+
+export const routes = [...rootModule.routes, ...authModule.routes];
+export const stores = [...authModule.stores];

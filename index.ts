aeeea5acea770946838b6a900cdc236fb3633755
h.saatkamp <helmuth.saatkamp@leverton.ai>
2019-04-11 14:27:02 +0200

    chore: code changes

--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
-import * as rootModule from './src/modules/root';
-import * as authModule from './src/modules/auth';
-
-export type State = authModule.State;
-
-export const routes = [...rootModule.routes, ...authModule.routes];
-export const stores = [...authModule.stores];
-
-export default {
-  routes,
-  stores
-};
+export * from './src/modules';
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -5,3 +5,8 @@ export type State = authModule.State;
 
 export const routes = [...rootModule.routes, ...authModule.routes];
 export const stores = [...authModule.stores];
+
+export default {
+  routes,
+  stores
+};

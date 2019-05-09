e331a4c4328841ba834e3d47321e31e5b983f5de
h.saatkamp <helmuth.saatkamp@leverton.ai>
2019-05-09 15:00:04 +0200

    feat: added react-snap support chore: update package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 // import * as Sentry from '@sentry/browser';
 import React from 'react';
-import { render } from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
 
-render(<App />, document.querySelector('#root'));
+const rootElement = document.getElementById('root') as HTMLDivElement;
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

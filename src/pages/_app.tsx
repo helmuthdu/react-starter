4eb6ae90369a33cc8ce96dc86a5f4df6f8ccf402
Helmuth Saatkamp <helmuth.saatkamp@nist.de>
2019-06-14 11:34:08 +0200

    feat: added react intl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,54 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { IntlProvider, addLocaleData } from 'react-intl';
 // modules
 import { stores, State } from '../modules';
 // store
 import createStore from '../stores';
 import * as rootStores from '../stores/modules';
 
+declare const window: any;
+
+// Register React Intl's locale data for the user's locale in the browser. This
+// locale data was added to the page by `pages/_document.js`. This only happens
+// once, on initial page load in the browser.
+if (typeof window !== 'undefined' && window.ReactIntlLocaleData) {
+  Object.keys(window.ReactIntlLocaleData).forEach(lang => {
+    addLocaleData(window.ReactIntlLocaleData[lang]);
+  });
+}
+
 export type AppState = State & rootStores.State;
 
 export default class extends App {
+  public static async getInitialProps({ Component, router, ctx }: any) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    // Get the `locale` and `messages` from the request object on the server.
+    // In the browser, use the same values that the server serialized.
+    const { req } = ctx;
+    const { locale, messages } = req || window.__NEXT_DATA__.props;
+    const initialNow = Date.now();
+
+    return { pageProps, locale, messages, initialNow };
+  }
+
   public render() {
-    const { Component, pageProps } = this.props;
+    // @ts-ignore
+    const { Component, pageProps, locale, messages, initialNow } = this.props;
+
     return (
       <Container>
-        <Provider store={createStore([...rootStores.stores, ...stores])}>
-          <Component {...pageProps} />
-        </Provider>
+        <IntlProvider locale={locale} messages={messages} initialNow={initialNow}>
+          <Provider store={createStore([...rootStores.stores, ...stores])}>
+            <Component {...pageProps} />
+          </Provider>
+        </IntlProvider>
       </Container>
     );
   }
--- wallaby.config.js
+++ wallaby.config.js
@@ -3,19 +3,13 @@ module.exports = function(wallaby) {
   process.env.BABEL_ENV = 'test';
   process.env.NODE_ENV = 'test';
   process.env.NODE_PATH +=
-    path.delimiter +
-    path.join(__dirname, 'node_modules') +
-    path.delimiter +
-    path.join(__dirname, 'node_modules/react-scripts/node_modules');
-  require('module').Module._initPaths();
+    path.delimiter + path.join(__dirname, 'node_modules') + require('module').Module._initPaths();
 
   return {
     files: [
-      { pattern: 'src/setupTests.ts', instrument: false },
       'src/**/*.+(ts|tsx|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)',
       '!src/**/__tests__/**/*.ts?(x)',
-      '!src/**/*.stories.ts?(x)',
-      '!src/registerServiceWorker.ts'
+      '!src/**/*.stories.ts?(x)'
     ],
 
     tests: ['src/**/__tests__/**/*.ts?(x)'],
@@ -36,9 +30,7 @@ module.exports = function(wallaby) {
     },
 
     setup: wallaby => {
-      const jestConfig = require('react-scripts/scripts/utils/createJestConfig')(p =>
-        require.resolve('react-scripts/' + p)
-      );
+      const jestConfig = require('./jest.config');
       Object.keys(jestConfig.transform || {}).forEach(
         k => ~k.indexOf('^.+\\.(js|jsx') && void delete jestConfig.transform[k]
       );

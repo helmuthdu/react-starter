9480230a3706fd4a362367b7372be9a8880a58de
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-09-30 22:51:49 +0200

    chore: code cleanup

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,14 @@
 require('dotenv').config();
 
+import { NextJS } from './nextjs';
 import { Server } from './server';
 
 const next = require('next');
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dir: './client', dev });
-const handle = app.getRequestHandler();
 
-app.prepare().then(async () => {
-  Server.bootstrap(handle).server.listen(port, () => {
+NextJS.prepare().then(async () => {
+  Server.bootstrap().server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
--- server/server.ts
+++ server/server.ts
@@ -1,44 +1,14 @@
-import * as accepts from 'accepts';
 import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
 import * as cors from 'cors';
 import * as errorHandler from 'errorhandler';
 import * as express from 'express';
-import { NextFunction, Request, Response } from 'express';
-import { readFileSync } from 'fs';
-import * as glob from 'glob';
 import * as helmet from 'helmet';
-import * as http from 'http';
 import * as methodOverride from 'method-override';
-import { UrlLike } from 'next-server/router';
-import * as path from 'path';
+import nextjs from './nextjs';
 
 import { ApiRoutes } from './routes';
 
-type RequestHandler = (req: http.IncomingMessage, res: http.ServerResponse, parsedUrl?: UrlLike) => Promise<void>;
-
-// Polyfill Node with `Intl` that has data for all locales.
-// See: https://formatjs.io/guides/runtime-environments/#server
-const IntlPolyfill = require('intl');
-Intl.NumberFormat = IntlPolyfill.NumberFormat;
-Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
-
-// Get the supported languages by looking for translations in the `locales/` dir.
-const supportedLanguages = glob.sync(path.resolve('./locales/*.json')).map(f => path.basename(f, '.json'));
-
-// We need to expose React Intl's locale data on the request for the user's
-// locale. This function will also cache the scripts by lang in memory.
-const localeDataCache = new Map();
-const getLocaleDataScript = locale => {
-  const lang = locale.split('-')[0];
-  if (!localeDataCache.has(lang)) {
-    localeDataCache.set(
-      lang,
-      readFileSync(require.resolve(`@formatjs/intl-relativetimeformat/dist/locale-data/${lang}.js`), 'utf8')
-    );
-  }
-  return localeDataCache.get(lang);
-};
 /**
  * The server.
  *
@@ -52,12 +22,11 @@ export class Server {
    * @method bootstrap
    * @static
    */
-  public static bootstrap(requestHandle: RequestHandler): Server {
-    return new Server(requestHandle);
+  public static bootstrap(): Server {
+    return new Server();
   }
 
   public server: express.Application;
-  public requestHandler: RequestHandler;
 
   /**
    * Constructor.
@@ -65,8 +34,7 @@ export class Server {
    * @class Server
    * @constructor
    */
-  constructor(requestHandler: RequestHandler) {
-    this.requestHandler = requestHandler;
+  constructor() {
     this.server = express();
 
     // configure application
@@ -76,17 +44,7 @@ export class Server {
     this.routes();
 
     // nextjs
-    this.server.get(
-      '*',
-      (req: Request & { locale: string; messages: { [key: string]: string }, localeDataScript: string }, res: Response, next: NextFunction) => {
-        const accept = accepts(req);
-        const locale = accept.language(accept.languages(supportedLanguages) as any) || 'en';
-        req.locale = locale;
-        req.localeDataScript = getLocaleDataScript(locale);
-        req.messages = require(path.resolve(`./locales/${locale}.json`));
-        return this.requestHandler(req, res);
-      }
-    );
+    this.server.get('*', nextjs());
   }
 
   /**
@@ -96,29 +54,13 @@ export class Server {
    * @method config
    */
   public config() {
-    // mount json form parser
     this.server.use(bodyParser.json({ limit: '2000kb' }));
+    this.server.use(bodyParser.urlencoded({ extended: true }));
 
-    // mount query string parser
-    this.server.use(
-      bodyParser.urlencoded({
-        extended: true
-      })
-    );
-
-    // mount override?
     this.server.use(helmet());
     this.server.use(cors());
     this.server.use(compression());
     this.server.use(methodOverride());
-
-    // catch 404 and forward to error handler
-    this.server.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-      err.status = 404;
-      next(err);
-    });
-
-    // error handling
     this.server.use(errorHandler());
   }
 

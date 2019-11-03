72599d0e32bf392ce519c0180a62c69cd0b53ad6
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-11-03 15:12:40 +0100

    chore: fix locale actions

--- src/stores/locale/actions.ts
+++ src/stores/locale/actions.ts
@@ -3,12 +3,12 @@ import { LocaleActionTypes } from './types';
 
 export const actionGetMessages = async (payload: string) => ({
   type: LocaleActionTypes.SET_LOCALE_MESSAGES,
-  payload: (await Http.get({ url: `/locales/${payload}.json` })).data
+  payload: { messages: (await Http.get({ url: `/locales/${payload}.json` })).data }
 });
 
 export const actionSetLocale = (payload: string) => ({
   type: LocaleActionTypes.SET_LOCALE_LANGUAGE,
-  payload
+  payload: { language: payload }
 });
 
 export default {

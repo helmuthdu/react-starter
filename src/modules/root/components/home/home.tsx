bdcaad56e7d9783d69e3d610bc424ada9e208694
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-11-11 09:57:10 +0100

    chore: example for react-intl

--- src/modules/root/components/home/home.tsx
+++ src/modules/root/components/home/home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { USER_URL, ROOT_URL } from '../../../urls';
 import logo from '../../assets/images/logo.svg';
 
@@ -8,10 +9,10 @@ export const Home = (props: Props) => (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit <code>src/App.js</code> and save to reload.
+        Edit <code>src/app.tsx</code> and save to reload.
       </p>
       <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
+        <FormattedMessage id="LEARN_REACT" />
       </a>
       Navigate to
       <div className="App-link" onClick={() => props.onLinkClick(ROOT_URL.ABOUT)} title="go to about page">

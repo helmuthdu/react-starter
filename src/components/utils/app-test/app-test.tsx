76016c55a607e2c0b97aa6a3937c8ea518185287
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-02-01 10:22:21 +0100

    chore: update package.json

--- src/components/utils/app-test/app-test.tsx
+++ src/components/utils/app-test/app-test.tsx
@@ -5,7 +5,7 @@ import { StoreProvider } from '../../../contexts/store/store.context';
 import { initialState, reducer } from '../../../stores';
 
 export const AppTest = ({ children }: any) => (
-  <IntlProvider locale="en">
+  <IntlProvider locale="en" onError={jest.fn()}>
     <StoreProvider initialState={initialState} reducer={reducer}>
       <MemoryRouter>{children}</MemoryRouter>
     </StoreProvider>
--- src/modules/user/components/sign-in/sign-in.tsx
+++ src/modules/user/components/sign-in/sign-in.tsx
@@ -1,5 +1,5 @@
 import React, { MouseEvent, SyntheticEvent } from 'react';
-import useForm from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 type FormValues = {
   email: string;

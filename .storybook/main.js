a04fad816c904a373e532b384576aa767868b782
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-02-01 12:17:03 +0100

    fix: storybook outdated config

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.story.[tj]sx'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs',
+    '@storybook/addon-links',
+  ],
+};

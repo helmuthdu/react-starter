be2732a5864788423927a4d8d465d1eca86489f7
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-02-19 10:05:59 +0100

    chore: minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -13,10 +13,5 @@ if (rootElement.hasChildNodes()) {
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// Sentry is an open-source error tracking tool that helps you monitor
-// and fix crashes in real time. Here we cover everything about the
-// product, the platform integrations, and our on-premise offering.
-// Sentry.init({ dsn: 'https://<key>@sentry.io/<project>' });

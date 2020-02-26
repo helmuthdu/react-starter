1d38706a7ec9662aac42cb736540fc586c2ac074
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-02-26 12:28:52 +0100

    chore: update type definition

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,15 +6,13 @@ interface Window {
   __REDUX_DEVTOOLS_EXTENSION__: object;
 }
 
-type Enum<E> = Record<keyof E, number | string> & { [P in keyof E]: string };
-
 type ValueOf<T> = T[keyof T];
 
-type Dictionary<T> = { [P in T[keyof T] | keyof T | string]: T };
-
-type DictionaryArray<T> = { [P in T[keyof T] | keyof T | string]: [T] };
-
 type Nullable<T> = T | null;
+
+type Dictionary<T> = Record<T[keyof T] | keyof T | string, T | T[keyof T] | any>;
+
+type DictionaryArray<T> = Record<T[keyof T] | keyof T | string, T[] | any[]>;
 
 type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]

937655965cb398aed7e19b104ba86358e1182a07
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-02-26 12:30:12 +0100

    chore: update type definition

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,10 +1,12 @@
 /// <reference types="react-scripts" />
 
-type Enum<E> = Record<keyof E, number | string> & { [k: number]: string };
-
 type ValueOf<T> = T[keyof T];
 
-type Dictionary<T, K extends keyof any> = { [P in K]: T };
+type Nullable<T> = T | null;
+
+type Dictionary<T> = Record<T[keyof T] | keyof T | string, T | T[keyof T] | any>;
+
+type DictionaryArray<T> = Record<T[keyof T] | keyof T | string, T[] | any[]>;
 
 type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends (infer U)[]

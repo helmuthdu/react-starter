6e4cf9e455782f8518254ad90dc2d52b61f90864
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-03-10 18:17:43 +0100

    chore: minor changes

--- client/pages/sign-in/index.tsx
+++ client/pages/sign-in/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { DefaultLayout } from '../../modules/user/layouts/default/default.layout';
 import SignInRoute from '../../modules/user/pages/sign-in/sign-in.page';
 
-const Page = () =>
+const Page = () => (
   <DefaultLayout>
     <SignInRoute />
-  </DefaultLayout>;
+  </DefaultLayout>
+);
 
 export default Page;

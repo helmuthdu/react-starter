cb8df0488c155a10a8fe532210bb6c0d5d146337
helmuthdu <1JnjuslI>
2020-07-24 12:14:22 +0200

    chore: update package.json

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,13 +1,11 @@
 require('@testing-library/jest-dom/extend-expect');
 require('mutationobserver-shim');
-require('@formatjs/intl-relativetimeformat/dist/locale-data/en');
-require('@formatjs/intl-relativetimeformat/polyfill');
 
 global.fetch = require('jest-fetch-mock');
 
 window.matchMedia =
   window.matchMedia ||
-  function() {
+  function () {
     return {
       matches: false,
       addListener: () => void 0,
@@ -17,6 +15,6 @@ window.matchMedia =
 
 window.requestAnimationFrame =
   window.requestAnimationFrame ||
-  function(callback) {
+  function (callback) {
     setTimeout(callback, 0);
   };

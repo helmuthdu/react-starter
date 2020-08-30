0cd858b070bb3946602d383c12edd018ac0a8475
helmuthdu <helmuthdu@gmail.com>
2020-08-30 20:07:34 +0200

    chore: update package.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,26 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../src/**/*.stories.[tj]sx'],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.s[ca]ss$/,
+      loader: 'style-loader!css-loader!resolve-url-loader!sass-loader'
+    });
+    // Return the altered config
+    return config;
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-knobs',
     '@storybook/addon-links',
-  ],
+    '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
+    '@storybook/addon-events',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y'
+  ]
 };

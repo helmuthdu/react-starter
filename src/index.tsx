22f6489992458ccf3e21e440404fdb89021acf31
helmuthdu <helmuthdu@gmail.com>
2020-10-24 09:48:39 +0200

    feat: replaced hot-reload by fast-refresh

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './app';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root') as HTMLDivElement;
-// Sentry.init({ dsn: 'https://examplePublicKey@o0.ingest.sentry.io/0' });
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

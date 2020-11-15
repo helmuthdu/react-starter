560e37497466ba608671af7da42d50ccd846659a
helmuthdu <helmuthdu@gmail.com>
2020-11-15 21:52:45 +0100

    chore: converted class into function component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,31 @@
 import { StoreProvider } from '@/stores';
-import App, { AppInitialProps } from 'next/app';
+import { AppInitialProps } from 'next/app';
 import { AppContext } from 'next/dist/pages/_app';
 import { IntlProvider } from 'react-intl';
 
 import '../styles/all.scss';
 
-export default class extends App {
-  public static async getInitialProps({ Component, ctx }: AppContext): Promise<AppInitialProps & Record<string, any>> {
-    let pageProps = {};
+const App = ({ Component, pageProps, locale, messages }: any) => {
+  return (
+    <StoreProvider logger={process.env.NODE_ENV === 'development'}>
+      <IntlProvider locale={locale} messages={messages}>
+        <Component {...pageProps} />
+      </IntlProvider>
+    </StoreProvider>
+  );
+};
 
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
+App.getInitialProps = async ({ Component, ctx }: AppContext): Promise<AppInitialProps & Record<string, any>> => {
+  let pageProps = {};
 
-    // Get the `locale` and `messages` from the request object on the server.
-    // In the browser, use the same values that the server serialized.
-    const { req } = ctx;
-    const { locale, messages } = req || window.__NEXT_DATA__.props;
-
-    return { pageProps, locale, messages };
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
   }
 
-  public render(): JSX.Element {
-    const { Component, pageProps, locale, messages } = this.props as any;
+  const { req } = ctx;
+  const { locale, messages } = req || window.__NEXT_DATA__.props;
 
-    return (
-      <StoreProvider logger={process.env.NODE_ENV === 'development'}>
-        <IntlProvider locale={locale} messages={messages}>
-          <Component {...pageProps} />
-        </IntlProvider>
-      </StoreProvider>
-    );
-  }
-}
+  return { pageProps, locale, messages };
+};
+
+export default App;

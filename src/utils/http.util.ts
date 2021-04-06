c9f9fdd247a4e76284f4d81748bfe3744cde3e1f
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-04-06 20:24:57 +0200

    feat: use logger util in http util

--- src/utils/http.util.ts
+++ src/utils/http.util.ts
@@ -1,37 +1,49 @@
 import fetch from 'isomorphic-unfetch';
+import Logger from './logger.util';
 
-type HttpOptions = {
+const log = (type: keyof typeof Logger, method: string, url: string, data: any, time: number) => {
+  const _url = url?.split('/') as string[];
+  const timestamp = Logger.getTimestamp();
+  Logger.groupCollapsed(`Http.${method?.toLowerCase()}('…/${_url[_url.length - 1]}')`, time);
+  Logger.setTimestamp(false);
+  Logger.info('url:', url);
+  Logger[type]('res:' as never, data);
+  Logger.setTimestamp(timestamp);
+  Logger.groupEnd();
+};
+
+type HttpParams = {
   body?: unknown;
   headers?: Record<string, unknown>;
   url: string;
 };
 
+type FetchParams = HttpParams & { method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' };
+
 type HttpResponse<T> = Partial<Response> & { data?: T; error?: unknown };
 
 export class Http {
-  static async get<T>(options: HttpOptions): Promise<HttpResponse<T>> {
+  static async get<T>(options: HttpParams): Promise<HttpResponse<T>> {
     return await this.fetch<T>({ method: 'GET', ...options });
   }
 
-  static async post<T>(options: HttpOptions): Promise<HttpResponse<T>> {
+  static async post<T>(options: HttpParams): Promise<HttpResponse<T>> {
     return await this.fetch<T>({ method: 'POST', ...options });
   }
 
-  static async put<T>(options: HttpOptions): Promise<HttpResponse<T>> {
+  static async put<T>(options: HttpParams): Promise<HttpResponse<T>> {
     return await this.fetch<T>({ method: 'PUT', ...options });
   }
 
-  static async patch<T>(options: HttpOptions): Promise<HttpResponse<T>> {
+  static async patch<T>(options: HttpParams): Promise<HttpResponse<T>> {
     return await this.fetch<T>({ method: 'PATCH', ...options });
   }
 
-  static async delete<T>(options: HttpOptions): Promise<HttpResponse<T>> {
+  static async delete<T>(options: HttpParams): Promise<HttpResponse<T>> {
     return await this.fetch<T>({ method: 'DELETE', ...options });
   }
 
-  private static async fetch<T>(
-    options: HttpOptions & { method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' }
-  ): Promise<HttpResponse<T>> {
+  private static async fetch<T>(options: FetchParams): Promise<HttpResponse<T>> {
     const { url, method, headers, body } = options;
 
     const req: RequestInit = {
@@ -43,14 +55,15 @@ export class Http {
       req.body = JSON.stringify(body);
     }
 
+    const time = Date.now();
     return fetch(url, req)
       .then(async (res: Response) => {
         const data: T = await res.json();
+        log('success', method, url, data, time);
         return { ...res, data };
       })
       .catch(error => {
-        console.error(error);
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        log('error', method, url, error, time);
         return { error };
       });
   }

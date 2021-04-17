e4aecf22edb1e45030ef6d8f92096a690e38957b
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-04-17 19:34:39 +0200

    fix: broken test

--- src/routes/__tests__/routes.test.tsx
+++ src/routes/__tests__/routes.test.tsx
@@ -1,14 +1,14 @@
 import { render } from '@testing-library/react';
 import React from 'react';
-import { MemoryRouter } from 'react-router';
 import AppRouter from '../index';
+import { StoreProvider } from '../../stores';
 
 describe('App router', () => {
   it('should match snapshot', () => {
     const { asFragment } = render(
-      <MemoryRouter>
+      <StoreProvider logger={process.env.NODE_ENV === 'development'}>
         <AppRouter routes={[]} />
-      </MemoryRouter>
+      </StoreProvider>
     );
     expect(asFragment()).toMatchSnapshot();
   });

1f557e1fb28f4042ac7a18e47daefb48a4e4b7bd
helmuthdu <helmuthdu@gmail.com>
2021-04-18 17:13:24 +0200

    fix: avoid rerender when loading translations

--- src/components/components/i18n/i18n-router.tsx
+++ src/components/components/i18n/i18n-router.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useMemo } from 'react';
+import isEqual from 'lodash/isEqual';
+import React, { useEffect } from 'react';
 import { IntlProvider } from 'react-intl';
 import { BrowserRouter, Redirect, Route } from 'react-router-dom';
-import { loadTranslationsAsync, localeState, SupportedLanguages } from '../../../stores/locale.store';
 import { useRecoilState } from 'recoil';
+import { loadTranslationsAsync, localeState, SupportedLanguages } from '../../../stores/locale.store';
 
 export const isLanguageSupported = (lang: SupportedLanguages): boolean =>
   Object.values(SupportedLanguages).includes(lang);
@@ -11,36 +12,37 @@ export const I18nRouter: React.FC = ({ children }) => {
   const [{ locale, messages }, setLocale] = useRecoilState(localeState);
 
   useEffect(() => {
-    loadTranslationsAsync(locale).then((messages = {}) => {
-      setLocale(state => ({ ...state, messages }));
+    loadTranslationsAsync(locale).then((res = {}) => {
+      if (!isEqual(messages, res)) {
+        setLocale(state => ({ ...state, messages: res }));
+      }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return useMemo(
-    () => (
-      <BrowserRouter>
-        <Route path="/:lang([a-zA-Z]{2}-[a-zA-Z]{2})">
-          {({ match, location }) => {
-            const { pathname } = location;
-            const lang: SupportedLanguages = (match?.params as any)?.lang ?? SupportedLanguages.English;
+  // Waiting translation to be loaded before processed
+  if (Object.keys(messages).length === 0) return null;
 
-            if (!pathname.includes(`/${lang}/`)) {
-              return <Redirect to={`/${lang}/`} />;
-            } else if (!isLanguageSupported(lang)) {
-              return <Redirect to={`/${SupportedLanguages.English}/`} />;
-            }
+  return (
+    <BrowserRouter>
+      <Route path="/:lang([a-zA-Z]{2}-[a-zA-Z]{2})">
+        {({ match, location }) => {
+          const { pathname } = location;
+          const lang: SupportedLanguages = (match?.params as any)?.lang ?? SupportedLanguages.English;
 
-            return (
-              <IntlProvider locale={lang} messages={messages}>
-                {children}
-              </IntlProvider>
-            );
-          }}
-        </Route>
-      </BrowserRouter>
-    ),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [messages]
+          if (!pathname.includes(`/${lang}/`)) {
+            return <Redirect to={`/${lang}/`} />;
+          } else if (!isLanguageSupported(lang)) {
+            return <Redirect to={`/${SupportedLanguages.English}/`} />;
+          }
+
+          return (
+            <IntlProvider locale={lang} messages={messages} onError={() => undefined}>
+              {children}
+            </IntlProvider>
+          );
+        }}
+      </Route>
+    </BrowserRouter>
   );
 };

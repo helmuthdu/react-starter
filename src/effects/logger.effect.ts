fad084439c28ea8dfe9b6fb221f933f5ba387655
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-05-26 11:15:30 +0200

    fix: linter errors

--- src/effects/logger.effect.ts
+++ src/effects/logger.effect.ts
@@ -1,28 +1,26 @@
 import { DefaultValue, RecoilState } from 'recoil';
 import { Logger } from '../utils';
 
-export const loggerEffect =
-  (name: string) =>
-  <T>({
-    onSet
-  }: {
-    node: RecoilState<T>;
-    trigger: 'set' | 'get';
+export const loggerEffect = (name: string) => <T>({
+  onSet
+}: {
+  node: RecoilState<T>;
+  trigger: 'set' | 'get';
 
-    // Call synchronously to initialize value or async to change it later
-    setSelf: (
-      param: T | DefaultValue | Promise<T | DefaultValue> | ((param: T | DefaultValue) => T | DefaultValue)
-    ) => void;
-    resetSelf: () => void;
+  // Call synchronously to initialize value or async to change it later
+  setSelf: (
+    param: T | DefaultValue | Promise<T | DefaultValue> | ((param: T | DefaultValue) => T | DefaultValue)
+  ) => void;
+  resetSelf: () => void;
 
-    // Subscribe callbacks to events.
-    // Atom effect observers are called before global transaction observers
-    onSet: (param: (newValue: T | DefaultValue, oldValue: T | DefaultValue) => void) => void;
-  }) => {
-    onSet((nextState, prevState) => {
-      Logger.groupCollapsed(name, 'EFFECT');
-      Logger.debug('PREV_STATE', JSON.parse(JSON.stringify(prevState)));
-      Logger.debug('NEXT_STATE', JSON.parse(JSON.stringify(nextState)));
-      Logger.groupEnd();
-    });
-  };
+  // Subscribe callbacks to events.
+  // Atom effect observers are called before global transaction observers
+  onSet: (param: (newValue: T | DefaultValue, oldValue: T | DefaultValue) => void) => void;
+}) => {
+  onSet((nextState, prevState) => {
+    Logger.groupCollapsed(name, 'EFFECT');
+    Logger.debug('PREV_STATE', JSON.parse(JSON.stringify(prevState)));
+    Logger.debug('NEXT_STATE', JSON.parse(JSON.stringify(nextState)));
+    Logger.groupEnd();
+  });
+};

ed9c6395c60521513897ff5ac814d199c6163504
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-05-28 13:16:06 +0200

    chore: update http util

--- src/utils/http.util.ts
+++ src/utils/http.util.ts
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 import { Logger } from './logger.util';
 
-type HttpRequestConfig = RequestInit & { id?: string };
+type HttpRequestConfig = RequestInit & { id?: string; latestOnly?: boolean; skipCustomHeaders?: boolean };
 
 const log = (type: keyof typeof Logger, url: string, req: RequestInit, res: unknown, time: number) => {
   const _url = url?.split('/') as string[];
@@ -21,32 +21,34 @@ const log = (type: keyof typeof Logger, url: string, req: RequestInit, res: unkn
 
 const activeRequests = {} as Record<string, { request: Promise<any>; controller: AbortController }>;
 
+const defaultHeaders = {
+  'Content-Type': 'application/json'
+};
+
 export class Http {
-  private static _headers: Record<string, string> = {
-    'Content-Type': 'application/json'
-  };
+  private static _headers: Record<string, string | number> = {};
 
-  static async get<T>(url: string, config?: HttpRequestConfig, latestOnly?: boolean): Promise<T> {
-    return await this._request<T>(url, { method: 'GET', ...config }, latestOnly);
+  static async get<T>(url: string, config?: HttpRequestConfig): Promise<T> {
+    return await this._request<T>(url, { method: 'GET', ...config });
   }
 
-  static async post<T>(url: string, config?: HttpRequestConfig, latestOnly?: boolean): Promise<T> {
-    return await this._request<T>(url, { method: 'POST', ...config }, latestOnly);
+  static async post<T>(url: string, config?: HttpRequestConfig): Promise<T> {
+    return await this._request<T>(url, { method: 'POST', ...config });
   }
 
-  static async put<T>(url: string, config?: HttpRequestConfig, latestOnly?: boolean): Promise<T> {
-    return await this._request<T>(url, { method: 'PUT', ...config }, latestOnly);
+  static async put<T>(url: string, config?: HttpRequestConfig): Promise<T> {
+    return await this._request<T>(url, { method: 'PUT', ...config });
   }
 
-  static async patch<T>(url: string, config?: HttpRequestConfig, latestOnly?: boolean): Promise<T> {
-    return await this._request<T>(url, { method: 'PATCH', ...config }, latestOnly);
+  static async patch<T>(url: string, config?: HttpRequestConfig): Promise<T> {
+    return await this._request<T>(url, { method: 'PATCH', ...config });
   }
 
-  static async delete<T>(url: string, config?: HttpRequestConfig, latestOnly?: boolean): Promise<T> {
-    return await this._request<T>(url, { method: 'DELETE', ...config }, latestOnly);
+  static async delete<T>(url: string, config?: HttpRequestConfig): Promise<T> {
+    return await this._request<T>(url, { method: 'DELETE', ...config });
   }
 
-  public static setHeaders(headers: Record<string, string | undefined>) {
+  public static setHeaders(headers: Record<string, string | number | undefined>): void {
     Object.entries(headers).forEach(([key, val]) => {
       if (val === undefined) {
         delete this._headers[key];
@@ -56,8 +58,14 @@ export class Http {
     });
   }
 
-  private static async _request<T>(url: string, config: HttpRequestConfig, latestOnly?: boolean): Promise<T> {
-    const { id = this.generateRequestId(config), ...cfg } = config;
+  private static async _request<T>(url: string, config: HttpRequestConfig): Promise<T> {
+    const {
+      id = this.generateRequestId(config),
+      headers = defaultHeaders,
+      latestOnly,
+      skipCustomHeaders,
+      ...cfg
+    } = config;
 
     if (activeRequests[id] && latestOnly) {
       activeRequests[id].controller.abort();
@@ -65,7 +73,15 @@ export class Http {
 
     if (!activeRequests[id]) {
       const controller = new AbortController();
-      const request = this._makeRequest(id, url, { ...cfg, signal: controller.signal });
+      const request = this._makeRequest(
+        id,
+        url,
+        Object.assign({}, cfg, {
+          body: JSON.stringify(config.body),
+          signal: controller.signal,
+          headers: skipCustomHeaders ? headers : { ...headers, ...this._headers }
+        }) as RequestInit
+      );
       activeRequests[id] = { request, controller };
     }
 
@@ -73,22 +89,15 @@ export class Http {
   }
 
   private static _makeRequest<T>(id: string, url: string, config: RequestInit): Promise<T> {
-    const cfg: RequestInit = Object.assign({}, config, {
-      headers: {
-        ...this._headers,
-        ...config.headers
-      }
-    });
-
     const time = Date.now();
-    return fetch(url, cfg)
+    return fetch(url, config)
       .then(async (res: Response) => {
         const data: T = await res.json();
-        log('success', url, cfg, data, time);
+        log('success', url, config, data, time);
         return data;
       })
       .catch(error => {
-        log('error', url, cfg, error, time);
+        log('error', url, config, error, time);
         throw error;
       })
       .finally(() => {

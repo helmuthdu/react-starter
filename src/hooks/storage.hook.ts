938d8d09b31da7a0b7d972c8f6ec7df9f14422b8
helmuthdu <helmuthdu@gmail.com>
2021-07-31 20:58:32 +0200

    feat: new storage hook

---
+++ src/hooks/storage.hook.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import { getStorageItem, Logger, setStorageItem } from '../utils';
+
+export const useStorage = <T>(key: string, defaultValue?: T, session = false): [T, (data: T) => void] => {
+  const [storage, setStorage] = useState(() => {
+    const item = getStorageItem<T>(key);
+    if (item === undefined && defaultValue) {
+      setStorageItem(key, defaultValue, session);
+      return defaultValue;
+    }
+    return item as T;
+  });
+
+  useEffect(() => {
+    Logger.info(`[STORAGE] watch('${key}')`, storage);
+    setStorageItem(key, storage);
+  }, [storage]);
+
+  return [storage, setStorage];
+};

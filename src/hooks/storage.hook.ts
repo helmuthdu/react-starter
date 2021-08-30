dfc16565052f5f46203870316411ceab593092bf
helmuthdu <helmuthdu@gmail.com>
2021-08-30 18:17:38 +0200

    chore: update storage hook

--- src/hooks/storage.hook.ts
+++ src/hooks/storage.hook.ts
@@ -1,26 +1,21 @@
 import { useEffect, useState } from 'react';
-import { getStorageItem, setStorageItem } from '../utils';
+import { getStorageItem, Logger, setStorageItem } from '../utils';
 
-type Dispatch<A> = (value: A) => void;
-type SetStateAction<S> = S | ((prevState: S) => S);
-
-export const useStorage = <T>(key: string, defaultValue?: T, session = false): [T, Dispatch<SetStateAction<T>>] => {
-  const getItem = () => {
+export const useStorage = <T>(key: string, defaultValue?: T, session = false): [T, (data: T) => void] => {
+  const [storage, setStorage] = useState(() => {
     const item = getStorageItem<T>(key);
-    if (!item && defaultValue) {
+    if (item === undefined && defaultValue) {
       setStorageItem(key, defaultValue, session);
       return defaultValue;
     }
-    return item;
-  };
-
-  const [storage, setStorage] = useState(getItem() as T);
+    return item as T;
+  });
 
   useEffect(() => {
+    Logger.info(`[STORAGE] watch('${key}')`, storage);
     setStorageItem(key, storage);
-  }, [key, storage]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [storage]);
 
   return [storage, setStorage];
 };
-
-export default useStorage;

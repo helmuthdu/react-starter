ee8473f2db1f662a49c95699b7df6483e317030c
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-10-28 09:17:10 +0200

    chore: performance improvements with new router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,86 +1,49 @@
-import React, { lazy, Suspense, useEffect } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { RouteObject, useRoutes } from 'react-router';
-import isEqual from 'lodash/isEqual';
 import { IntlProvider } from 'react-intl';
-import { Http } from '../utils';
-import { useStorage } from '../hooks/storage.hook';
 import { BrowserRouter } from 'react-router-dom';
-
-export type Locale = typeof locales[keyof typeof locales];
-export type LocaleMessages = { locale: Locale; messages: Record<string, any> };
-
-export const locales = {
-  english: 'en-US'
-} as const;
-
-export const isLanguageSupported = (locale: Locale): boolean => Object.values(locales).includes(locale);
-
-const loadTranslationsAsync = async (locale: Locale): Promise<Record<string, string> | undefined> =>
-  await Http.get<Record<string, string>>(`${process.env.PUBLIC_URL}/locales/${locale}.json`);
+import { addLocaleToRoutePath, isLanguageSupported, Locale, useLocale } from '../locales';
 
 const AppI18n: React.FC<{ locale: Locale }> = ({ locale, children }) => {
-  const [localeStorage, setLocaleStorage] = useStorage<LocaleMessages>('locale', { locale, messages: {} });
-
-  useEffect(() => {
-    loadTranslationsAsync(locale).then((res = {}) => {
-      if (!isEqual(localeStorage.messages, res)) {
-        setLocaleStorage({ locale, messages: res });
-      }
-    });
-    // eslint-disable-next-line
-  }, []);
+  const [localeStorage] = useLocale(locale);
 
   return (
-    <IntlProvider locale={locale} messages={localeStorage.messages} onError={() => undefined}>
+    <IntlProvider locale={localeStorage.locale} messages={localeStorage.messages} onError={() => undefined}>
       {children}
     </IntlProvider>
   );
 };
 
 const AppRoutes: React.FC<{ routes: RouteObject[] }> = ({ routes }) => {
-  const locale = window.location.pathname.split('/')[1] as Locale;
-
-  useEffect(() => {
-    if (!locale || !isLanguageSupported(locale)) {
-      window.location.href = `/${locales.english}/`;
-    }
-    // eslint-disable-next-line
-  }, []);
-
-  const updatePath = (route: RouteObject) => {
-    if (route.path?.includes(locale) || route.index) {
-      return route;
-    }
-
-    if (route.children && route.children.length > 0) {
-      route.children = route.children.map(updatePath);
-    }
-
-    route.path = `/${locale}/${route.path?.startsWith('/') ? route.path?.substring(1) : route.path}`;
-    return route;
-  };
-
   const NotFoundRoute = lazy(() => import('./not-found/not-found.route'));
 
   const component = useRoutes([
-    ...routes.map(updatePath),
+    ...routes.map(addLocaleToRoutePath),
+    {
+      path: 'not-found',
+      element: <NotFoundRoute />
+    },
     {
       path: '*',
       element: <NotFoundRoute />
     }
   ]);
 
-  return (
-    <AppI18n locale={locale}>
-      <Suspense fallback={null}>{component}</Suspense>
-    </AppI18n>
-  );
+  return <Suspense fallback={null}>{component}</Suspense>;
 };
 
 export const AppRouter: React.FC<{ routes: RouteObject[] }> = ({ routes }) => {
+  const locale: any = window.location.pathname.split('/')[1];
+
+  if (!isLanguageSupported(locale) && locale !== 'not-found') {
+    window.location.href = `/not-found`;
+  }
+
   return (
     <BrowserRouter>
-      <AppRoutes routes={routes} />
+      <AppI18n locale={locale}>
+        <AppRoutes routes={routes} />
+      </AppI18n>
     </BrowserRouter>
   );
 };

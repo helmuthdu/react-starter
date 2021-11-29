dbe391d1e37539e9cd1feecc5a0f7b247cf9b042
helmuthdu <helmuthdu@gmail.com>
2021-11-29 10:00:12 +0100

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
+    'plugin:@next/next/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:cypress/recommended',
     'plugin:prettier/recommended',

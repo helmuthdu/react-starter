41cd980db1eccc9129d79beb1caf94c17d79e349
helmuthdu <helmuthdu@gmail.com>
2022-07-17 22:13:56 +0200

    chore: code cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import React from '@vitejs/plugin-react';
 import { join } from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config = {
   plugins: [React()],
   resolve: {
     alias: {
       '@': join(__dirname, 'src')
     }
+  },
+  css: {
+    postcss: {
+      plugins: [require('postcss-import'), require('postcss-nesting'), require('autoprefixer')]
+    }
   }
-});
+};
+
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  config.css.postcss.plugins.push(require('cssnano'));
+}
+
+export default defineConfig(config);

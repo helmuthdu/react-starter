4904e5bde15676e8e48d7992cfd279bc4ece2493
helmuthdu <helmuthdu@gmail.com>
2022-07-22 09:40:54 +0200

    chore: update postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,11 @@ import React from '@vitejs/plugin-react';
 import { join } from 'path';
 import { defineConfig } from 'vite';
 
-const config = {
+export default defineConfig({
   plugins: [React()],
-  css: {
-    postcss: {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      plugins: [require('precss')({ stage: 0 })]
-    }
-  },
   resolve: {
     alias: {
       '@': join(__dirname, 'src')
     }
   }
-};
-
-if (process.env.NODE_ENV === 'production') {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  config.css.postcss.plugins.push(require('cssnano'));
-}
-
-export default defineConfig(config);
+});

42d6acf16ca8e40472b90639743b50ce01774c27
Helmuth Saatkamp <helmuthdu@gmail.com>
2022-08-30 15:51:38 +0200

    chore: minor changes

--- src/hooks/promise.hook.ts
+++ src/hooks/promise.hook.ts
@@ -1,20 +1,20 @@
 import { useRef } from 'react';
 
 export const usePromise = <T>(fn: (...args: any) => Promise<T>, defaultValue: T = null as any) => {
-  const result = useRef<T>(defaultValue);
+  const data = useRef<T>(defaultValue);
   const loading = useRef(false);
   const error = useRef<unknown>(null);
   const run = async (...args: any) => {
     loading.current = true;
     error.current = null;
-    result.current = defaultValue;
+    data.current = defaultValue;
     try {
-      result.current = await fn(...args);
+      data.current = await fn(...args);
     } catch (err) {
       error.current = err;
     } finally {
       loading.current = false;
     }
   };
-  return { result, loading, error, run };
+  return { data, loading, error, run };
 };

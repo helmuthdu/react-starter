5b2493152417e1b4a59ae506240add3b83bb5a30
Helmuth Saatkamp <helmuthdu@gmail.com>
2023-12-01 13:12:20 +0100

    chore: update utils

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -53,7 +53,7 @@ const getTimestamp = (): string => new Date().toISOString().split('T')[1].substr
 
 const print = (level: LoggerLevelKey, color: keyof typeof COLORS, ...args: any[]) => {
   const { logLevel, prefix, remote, timestamp } = state;
-  const type = (['DEBUG', 'SUCCESS'].includes(level) ? 'log' : level.toLowerCase()) as keyof Console;
+  const type = ['DEBUG', 'SUCCESS'].includes(level) ? 'log' : level.toLowerCase();
 
   if (logLevel > LogLevel[level]) return;
 
@@ -79,7 +79,7 @@ const print = (level: LoggerLevelKey, color: keyof typeof COLORS, ...args: any[]
   }
 
   stdout.push('color: inherit;', ...args);
-  (console[type].apply as any)(null, stdout);
+  (console as any)[type].apply(null, stdout);
 
   if (remote.handler) {
     if (remote.logLevel > LogLevel[level]) return;
@@ -148,8 +148,7 @@ export const Logger = {
     if (logLevel > LogLevel.SUCCESS) return;
 
     const elapsed = Math.floor(Date.now() - time);
-    const colorMode =
-      window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'PREFIX_DM' : 'PREFIX';
+    const colorMode = window.matchMedia?.('(prefers-color-scheme: dark)').matches ? 'PREFIX_DM' : 'PREFIX';
 
     console.groupCollapsed(
       `%c${label}%c${prefix ? `${prefix}` : ''}%c${timestamp ? `${getTimestamp()}` : ''}%c${text} %c${
